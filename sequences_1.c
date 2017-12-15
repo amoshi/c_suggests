6cf6bddcfadc6590cdecaf31727aa08b091c9269
amoshi <amoshi.mandrakeuser@gmail.com>
2017-12-16 02:56:03 +0300

    add sequence: task from mephi

--- sequences_1.c
+++ sequences_1.c
@@ -1,4 +1,4 @@
-//Дана последовательность из n целых элементов. Сформировать новую последовательность, в которой сначала расположены в порядке возрастания все положительные элементы исходной последовательности, а затем в порядке убывания все отрицательные. Все последовательности: исходную и полученную вывести на экран.
+// Дана последовательность из n целых элементов. Сформировать новую последовательность, состояющую из минимальных цифр элементов упорядоченной по убыванию исходной последовательности. Все последовательности: исходную и полученную вывести на экран.
 
 #include <stdio.h>
 
@@ -19,7 +19,7 @@ void sort_todown ( int *a, int n )
 	 }
 }
 
-show_element( int *b, int f )
+void show_element( int *b, int f )
 {
 	 int i;
 	 printf ("length=%d\n",f);
@@ -29,7 +29,7 @@ show_element( int *b, int f )
 	 }
 }
 
-get_element(int *b, int c)
+void get_element(int *b, int c)
 {
 	int i;
 	for ( i=0; i<c; i++ )
@@ -39,13 +39,26 @@ get_element(int *b, int c)
 	}
 }
 
-void get_min_element ( int *in, int *b,  int c )
+void get_min_element ( int *a, int *b,  int c )
 {
+	int i;
 	for ( i=0; i<c; i++)
 	{
-		int tmp = a[i];
-		int min = tmp;
-		for ( j=0; j<
+		int tmp = 1;
+		int min = a[i];
+		int y = 1;
+		int j;
+		//puts("----");
+		tmp = a[i]/y %10;
+		for ( j=0; tmp>0; j++ )
+		{
+			//printf("tmp=%d\n", tmp);
+			if ( min > tmp)
+				min = tmp;
+			y=y*10;
+			tmp = a[i]/y %10;
+		}
+		b[i]=min;
 	}
 }
 
@@ -76,9 +89,11 @@ int main()
 
 
 		puts("");
+		puts("----- array -----");
+		show_element(in,c);
 		puts("");
 		puts("----- result -----");
-		show_element(in,c);
+		show_element(b,c);
 		puts("----- ------ -----");
 	}
 }
