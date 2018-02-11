a408f3cbfe0aff3687cc737f38c7f15b5024698a
amoshi <amoshi.mandrakeuser@gmail.com>
2018-02-11 17:36:04 +0300

    working fib

--- fib.c
+++ fib.c
@@ -11,6 +11,18 @@ typedef struct seq
 	size_t m;
 } seq;
 
+typedef struct sseq
+{
+	seq *seq;
+	size_t n;
+} sseq;
+
+typedef struct uintstruct
+{
+	int64_t *fib;
+	size_t n;
+} uintstruct;
+
 size_t string_to_int( char *str, size_t l, seq *st )
 {
 	uint64_t i, j, k;
@@ -23,7 +35,7 @@ size_t string_to_int( char *str, size_t l, seq *st )
 		cur += strcspn(cur, "0123456789");
 		if ( cur == str+l )
 			break;
-		printf("%"PRIu64"/%zu: %p/%p/%p: %s\n", i, l, cur, curswp, str+l, cur);
+		//printf("%"PRIu64"/%zu: %p/%p/%p: %s\n", i, l, cur, curswp, str+l, cur);
 		cur += strcspn(cur, " \t\n\0") + 1;
 	}
 	cur = str;
@@ -31,7 +43,7 @@ size_t string_to_int( char *str, size_t l, seq *st )
 	for ( j=0; j<i; j++ )
 	{
 		cur += strcspn(cur, "0123456789");
-		printf("%lld\n", atoll(cur));
+		//printf("%lld\n", atoll(cur));
 		seq[j]=atoll(cur);
 		cur += strcspn(cur, " \t\n\0") + 1;
 	}
@@ -50,38 +62,135 @@ void input (seq **st)
 //	res = scanf("%s", arg);
 //	printf("%d, %s\n", res, arg);
 	fgets(arg, ARG_INPUT_MAXSIZE, stdin);
-	printf("%s\n", arg);
+	//printf("%s\n", arg);
 	string_to_int( arg, strlen(arg), st[0] );
 }
 
-void erase (seq **st)
+void erase_seq (seq *st)
+{
+	free( st->s );
+	free( st );
+}
+
+void erase_sseq ( sseq *sst )
 {
 	uint64_t i;
-	free( st[0]->s );
-	free( st[0] );
-	st = NULL;
+	for (i=0; i<sst->n; i++)
+		free( sst->seq[i].s );
+	free(sst);
 }
 
 void output(seq *st)
 {
 	int64_t i;
+	//printf("output: %zu\n", st->m);
 	for ( i=0; i<st->m; i++ )
 	{
-		printf(" %"PRId64" ", st->s[i]);
+		//printf("output element %"PRId64"\n", i);
+		printf("%5"PRId64" ", st->s[i]);
+	}
+	puts("");
+}
+
+void soutput(sseq *sst, seq *st)
+{
+	int64_t i;
+	for ( i=0; i < sst->n; i++ )
+	{
+		//printf("output element %"PRId64"\n", i);
+		printf("%6"PRId64": ", st->s[i]);
+		output(sst->seq+i);
 	}
 }
 
-void action(seq *st)
+int64_t max(seq *st)
 {
+	uint64_t i;
+	int64_t max = st->s[0];
+	for ( i=1; i<st->m; i++ )
+	{
+		if ( max < st->s[i] )
+			max=st->s[i];
+	}
+	return max;
+}
+
+uintstruct* gen_fib(int64_t max)
+{
+	//printf("max_fib: %"PRId64"\n", max);
+	uint64_t i, j;
+	int64_t tmp0 = 0, tmp1 = 1, tmp2 = 0, tmpcur = 0;
+	for ( i=2; tmp1<max; tmpcur=tmp0 + tmp1, tmp0=tmp1, tmp1=tmpcur, i++ /*, printf("tmp=%"PRId64"/%"PRId64"\n", tmpcur, max)*/ );
+	int64_t *fib = malloc(sizeof(int64_t)*i);
+	fib[0]=0;
+	fib[1]=1;
+	for ( j=2; j<i; j++ )
+	{
+		fib[j]=fib[j-1]+fib[j-2];
+		//printf("%"PRIu64":::%"PRId64"\n", j, fib[j]);
+	}
+	uintstruct *fs = malloc ( sizeof(uintstruct) );
+	fs->fib = fib;
+	fs->n = j;
+	return fs;
+}
+
+sseq* make_matrix (seq *st, uintstruct *fs)
+{
+	int64_t i, j;
+	sseq *sst;
+	//printf("m=%zu, fib_n=%zu\n", st->m, fs->n);
+	sst = malloc( sizeof(sseq) );
+	sst->n = st->m;
+	sst->seq = malloc ( sizeof(seq) * st->m );
+	for ( j=0; j<st->m; j++ )
+	{
+		int64_t count = st->s[j];
+		uint64_t k;
+		//printf("%"PRId64"\n",  st->s[j]);
+		for ( i=fs->n-1, k=0; count>=0 && i>0; i-- )
+		{
+			if ( count - fs->fib[i] >= 0 )
+			{
+				count -= fs->fib[i];
+				//printf("%"PRId64": %"PRId64", count=%"PRId64"\n", st->s[j], fs->fib[i], count);
+				k++;
+			}
+		}
+
+		sst->seq[j].s = malloc (sizeof(int64_t) * k);
+		count = st->s[j];
+		sst->seq[j].m = k;
+		for ( i=fs->n-1, k=0; count>=0 && i>0; i-- )
+		{
+			if ( count - fs->fib[i] >= 0 )
+			{
+				count -= fs->fib[i];
+				sst->seq[j].s[k] = fs->fib[i];
+				k++;
+			}
+		}
+	}
+	return sst;
+}
+
+void action(seq *st, sseq **sst)
+{
+	int64_t mmax = max(st);
+	uintstruct *fs = gen_fib(mmax);
+	sst[0] = make_matrix(st, fs);
 }
 
 int main()
 {
 	seq *st = NULL;
+	sseq *sst = NULL;
 	input (&st);
-	puts("out");
+	action (st, &sst);
+	printf("input numbers\n");
 	output (st);
-	action (st);
-	output (st);
-	//erase (&st);
+	printf("output matrix\n");
+	soutput (sst, st);
+	erase_seq (st);
+	erase_sseq (sst);
 }
