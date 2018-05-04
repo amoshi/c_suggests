8835d4f158cd043d889fd28510b29c7a7dca07d9
amoshi <amoshi.mandrakeuser@gmail.com>
2018-05-04 19:57:52 +0300

    add l2

--- l2.asm
+++ l2.asm
@@ -6,32 +6,35 @@ wrd1	dw	123
 wrd2	dw	10
 .code
 start:            
-                xor eax, eax
-                xor eax, eax
-                xor ecx, ecx
-                xor edx, edx
-                xor ebx, ebx
-                
-                
-		mov	ax,wrd1
-		mov	bx,wrd2
-		cmp	wrd2,0
-		je	exit
-		idiv	bx
-		mov	ax,dx
+	mov	ax,@data
+	mov	ds,ax
 
-                mov	cl,16
+	xor eax, eax
+	xor eax, eax
+	xor ecx, ecx
+	xor edx, edx
+	xor ebx, ebx
+	
+	
+	mov	ax,wrd1
+	mov	bx,wrd2
+	cmp	wrd2,0
+	je	exit
+	idiv	bx
+	mov	ax,dx
+	
+	mov	cl,16
 vertelka:
-                rol	ax,1
-		mov	bx,ax
-		and	bx,0000000000000111b
-		cmp	bx,000b
-			
-		jne	continue
-		inc	dx
+	rol	ax,1
+	mov	bx,ax
+	and	bx,0000000000000111b
+	cmp	bx,000b
+		
+	jne	continue
+	inc	dx
 		
 continue:
-		loop vertelka
+	loop vertelka
 
 exit:
 	mov	ax, 4c00h
