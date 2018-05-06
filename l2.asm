a320123e2d7788600a91c8354c9202716579a28c
amoshi <amoshi.mandrakeuser@gmail.com>
2018-05-07 00:21:19 +0300

    fix counter l2.asm

--- l2.asm
+++ l2.asm
@@ -1,42 +1,56 @@
 .model	small
-.stack 100h
+.stack	100h
 .386
 .data
-wrd1	dw	123
-wrd2	dw	10
+;wrd1	dw	1000101000100100b
+wrd1	dw	0000000000000000b
+wrd2	dw	65000
+res	dw	0
 .code
-start:            
+start:
 	mov	ax,@data
 	mov	ds,ax
 
-	xor eax, eax
-	xor eax, eax
-	xor ecx, ecx
-	xor edx, edx
-	xor ebx, ebx
-	
-	
+	xor	eax,eax
+	xor	ecx,ecx
+	xor	edx,edx
+	xor	ebx,ebx
+
+
 	mov	ax,wrd1
 	mov	bx,wrd2
-	cmp	wrd2,0
+	cmp	bx,0
 	je	exit
-	idiv	bx
+	div	bx
 	mov	ax,dx
-	
+	xor	dx,dx
+
 	mov	cl,16
 vertelka:
-	rol	ax,1
+
 	mov	bx,ax
 	and	bx,0000000000000111b
 	cmp	bx,000b
-		
 	jne	continue
+
+	dec	cl
+	dec	cl
+	dec	cl
+	cmp	cl,0
+	jl	exit
 	inc	dx
-		
+	shr	ax,3
+	jmp	vertelka
+
 continue:
-	loop vertelka
+	dec	cl
+	cmp	cl,0
+	jl	exit
+	shr	ax,1
+	jmp	vertelka
 
 exit:
-	mov	ax, 4c00h
+	mov	res,dx
+	mov	ax,4c00h
 	int	21h
 	end	start
